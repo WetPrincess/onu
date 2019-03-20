e7de833fd0a471f8cd78ca4d1ffb418ec6e37afd
WetPrincess <thies.lapp@mailbox.org>
2019-03-20 20:36:49 +0100

    Coding Coding Coding

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 from classes import *
+import random
 
 
-# =============
-# Deckcreation
-# =============
-
+# Deckcreation und Shuffle
 deck = []
+spieler01 = []
+spieler02 = []
 
-for cards in range(1,11):
-    deck.append(Card({"Red"}, {cards}))
-    deck.append(Card({"Green"}, {cards}))
-    deck.append(Card({"Blue"}, {cards}))
-    deck.append(Card({"Yellow"}, {cards}))
+for i in range(1,11):
+    deck.append(Card({"Red"}, {i}))
+    deck.append(Card({"Green"}, {i}))
+    deck.append(Card({"Blue"}, {i}))
+    deck.append(Card({"Yellow"}, {i}))
+
+# random.shuffle(deck)
+
 
 # Test ob alle Objekte angekommen sind
-for printout in range(0,40):
-    print(deck[printout].color, deck[printout].num)
+# for i in range(0,40):
+#     print(deck[i].color, deck[i].num)
 
+print("---")
+spieler01.extend(deck[0:5])
+del deck[0:5]
+print(spieler01[0].color)
+
+spieler02.extend(deck[0:5])
+del deck[0:5]
+print(spieler02[0].color)
